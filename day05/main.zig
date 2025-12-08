9548e1585f523ee22c9d8d7bd6d2a75e690c6661
Alex Miranda <alexmirandamoraes@gmail.com>
2025-12-08 17:02:08 +0100

    day05 part2

--- day05/main.zig
+++ day05/main.zig
@@ -8,6 +8,18 @@ const Allocator = std.mem.Allocator;
 const Reader = std.Io.Reader;
 const example = @embedFile("example.txt");
 
+const Range = struct {
+    start: u64,
+    end: u64,
+
+    fn compare(_: void, lhs: Range, rhs: Range) std.math.Order {
+        return switch (std.math.order(lhs.start, rhs.start)) {
+            .eq => std.math.order(lhs.end, rhs.end),
+            else => |cmp| cmp,
+        };
+    }
+};
+
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
@@ -27,11 +39,14 @@ pub fn main() !void {
     var reader = std.fs.File.reader(input_file, &read_buf);
     const answer_p1 = try countFreshIngredients(ally, &reader.interface);
     try stdout.print("Part 1: {d}\n", .{answer_p1});
+
+    try reader.seekTo(0);
+    const answer_p2 = try totalFreshIngredients(ally, &reader.interface);
+    try stdout.print("Part 2: {d}\n", .{answer_p2});
     try stdout.flush();
 }
 
 fn countFreshIngredients(ally: Allocator, reader: *Reader) !usize {
-    const Range = struct { start: u64, end: u64 };
     var ranges: std.ArrayList(Range) = .empty;
     defer ranges.deinit(ally);
 
@@ -65,6 +80,36 @@ fn countFreshIngredients(ally: Allocator, reader: *Reader) !usize {
     return count;
 }
 
+fn totalFreshIngredients(ally: Allocator, reader: *Reader) !usize {
+    var ranges: std.PriorityQueue(Range, void, Range.compare) = .init(ally, {});
+    defer ranges.deinit();
+
+    while (reader.takeDelimiterExclusive('\n')) |line| : (reader.toss(1)) {
+        if (line.len == 0) break;
+        var it = std.mem.splitScalar(u8, line, '-');
+        const start = try std.fmt.parseUnsigned(u64, it.next().?, 10);
+        const end = try std.fmt.parseUnsigned(u64, it.next().?, 10);
+        try ranges.add(.{ .start = start, .end = end });
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+
+    var count: usize = 0;
+    var prev: Range = ranges.remove();
+    while (ranges.removeOrNull()) |curr| {
+        if (curr.start <= prev.end) {
+            prev.end = @max(prev.end, curr.end);
+            continue;
+        }
+        count += prev.end - prev.start + 1;
+        prev.start = curr.start;
+        prev.end = curr.end;
+    }
+    count += prev.end - prev.start + 1;
+    return count;
+}
+
 test "part 1" {
     var reader: Reader = .fixed(example);
     const answer = try countFreshIngredients(testing.allocator, &reader);
@@ -72,5 +117,7 @@ test "part 1" {
 }
 
 test "part 2" {
-    return error.SkipZigTest;
+    var reader: Reader = .fixed(example);
+    const answer = try totalFreshIngredients(testing.allocator, &reader);
+    try expectEqual(14, answer);
 }
