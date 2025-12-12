38d56fb2803c86cceb13d22e896281a1445d0cff
Alex Miranda <alexmirandamoraes@gmail.com>
2025-12-13 00:29:11 +0100

    day09 part1

--- day09/main.zig
+++ day09/main.zig
@@ -4,8 +4,9 @@ const assert = std.debug.assert;
 const panic = std.debug.panic;
 const testing = std.testing;
 const expectEqual = std.testing.expectEqual;
+const Allocator = std.mem.Allocator;
 const Reader = std.Io.Reader;
-const Writer = std.Io.Writer;
+const example = @embedFile("example.txt");
 
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
@@ -18,12 +19,44 @@ pub fn main() !void {
     };
     defer input_file.close();
 
-    try stdout.print("All your {s} are belong to us.\n", .{"codebase"});
+    var read_buf: [8192]u8 = undefined;
+    var reader = std.fs.File.reader(input_file, &read_buf);
+
+    var gpa: std.heap.GeneralPurposeAllocator(.{ .safety = true }) = .init;
+    defer _ = gpa.deinit();
+    const ally = gpa.allocator();
+
+    const answer_p1 = try largestArea(ally, &reader.interface);
+    try stdout.print("Part 1: {d}\n", .{answer_p1});
     try stdout.flush();
 }
 
+fn largestArea(ally: Allocator, reader: *Reader) !u64 {
+    const Loc = struct { x: u64, y: u64 };
+    var red_tiles: std.ArrayList(Loc) = .empty;
+    defer red_tiles.deinit(ally);
+
+    var largest_rect: u64 = 0;
+    while (try reader.takeDelimiter('\n')) |line| {
+        if (line.len == 0) break;
+        var it = std.mem.splitAny(u8, line, ",\n");
+        const x = try std.fmt.parseUnsigned(u64, it.next().?, 10);
+        const y = try std.fmt.parseUnsigned(u64, it.next().?, 10);
+        // print("{d},{d}\n", .{ x, y });
+        for (red_tiles.items) |prev| {
+            const dx = 1 + if (x >= prev.x) x - prev.x else prev.x - x;
+            const dy = 1 + if (y >= prev.y) y - prev.y else prev.y - y;
+            largest_rect = @max(largest_rect, dx * dy);
+        }
+        try red_tiles.append(ally, .{ .x = x, .y = y });
+    }
+    return largest_rect;
+}
+
 test "part 1" {
-    return error.SkipZigTest;
+    var reader: std.Io.Reader = .fixed(example);
+    const answer = largestArea(testing.allocator, &reader);
+    try expectEqual(50, answer);
 }
 
 test "part 2" {
